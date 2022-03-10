5fcdad86040fcb8ee3378a36682aec39f3f293b5
Derek Higgins <derekh@redhat.com>
2022-03-10 11:17:33 -0500

    Slight deploy adjustment

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-cd /home/derekh/ofcir
+cd $(dirname $0)
 
 podman build -f Dockerfile -t testyimage:op
 podman push --authfile /opt/dev-scripts/pull_secret.json localhost/testyimage:op virthost.ostest.test.metalkube.org:5000/localimages/testimage:op
 oc -n openshift-machine-api delete --grace-period=1 pod/operator || true
-oc -n openshift-machine-api apply -f operator.yaml
+oc -n openshift-machine-api apply -f deploy.yaml
 
 
