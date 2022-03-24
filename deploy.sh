b84b91163e7ba926a803050b603dc6ca526abd32
Derek Higgins <derekh@redhat.com>
2022-03-24 07:09:41 -0400

    Rename the pod to "ofcir"

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ podman push --authfile /opt/dev-scripts/pull_secret.json localhost/testyimage:op
 oc get project ofcir || oc new-project ofcir
 
 DATE=$(date +%s)
-oc logs operator -c operator > logs/operator.${DATE}.log
-oc logs operator -c serve > logs/serve.${DATE}.log
+oc logs ofcir -c operator > logs/operator.${DATE}.log || true
+oc logs ofcir -c serve > logs/serve.${DATE}.log || true
 
 
-oc -n ofcir delete --grace-period=1 pod/operator || true
+oc -n ofcir delete --grace-period=1 pod/ofcir || true
 oc -n ofcir apply -f deploy.yaml
 
 
