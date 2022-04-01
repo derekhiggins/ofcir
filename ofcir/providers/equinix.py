c55d04f806355a4d1d67728124286224452b9a09
Derek Higgins <derekh@redhat.com>
2022-04-01 06:15:01 -0400

    Bump up the safety

--- ofcir/providers/equinix.py
+++ ofcir/providers/equinix.py
@@ -58,7 +58,7 @@ class Equinix(Base):
         else:
             logger.debug('Creating new node %s (%s)'%(devicename, count))
             # TODO: remove safetly when we are sure this wont accidently create a gazillion BM servers
-            if count > 10:
+            if count > 30:
                 logger.error('SAFETY: TOO MANY SERVERS: %s'%count)
                 return
 
